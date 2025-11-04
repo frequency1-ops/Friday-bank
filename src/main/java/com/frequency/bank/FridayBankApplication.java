4279040615c262f823a00ad187522178dfcdbec8
Salim_Juma_001 <87000813+frequency1-ops@users.noreply.github.com>
2025-11-04 18:09:55 +0300

    initial commit

---
+++ src/main/java/com/frequency/bank/FridayBankApplication.java
@@ -0,0 +1,13 @@
+package com.frequency.bank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FridayBankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FridayBankApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/frequency/bank/FridayBankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.frequency.bank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FridayBankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

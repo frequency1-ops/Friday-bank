b1bdeaa59da0b79a061efe758bff196c7c14a30f
Salim_Juma_001 <87000813+frequency1-ops@users.noreply.github.com>
2025-11-08 18:58:24 +0300

    added more entities

--- src/main/java/com/frequency/bank/entities/Branch.java
+++ src/main/java/com/frequency/bank/entities/Branch.java
@@ -1,5 +1,9 @@
 package com.frequency.bank.entities;
 
+import jakarta.persistence.Entity;
+
+@Entity
 public class Branch {
+	boolean sw = true;
 
 }
--- src/main/java/com/frequency/bank/entities/Loan.java
+++ src/main/java/com/frequency/bank/entities/Loan.java
@@ -1,5 +1,8 @@
 package com.frequency.bank.entities;
 
+import jakarta.persistence.Entity;
+
+@Entity
 public class Loan {
 
 }

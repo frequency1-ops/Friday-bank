b14fce1a71ccd54c62b3bbfc28d0c20415d02796
Salim_Juma_001 <87000813+frequency1-ops@users.noreply.github.com>
2025-11-12 16:23:56 +0300

    added id in dto conversions

--- src/main/java/com/frequency/bank/dtos/CustomerDto.java
+++ src/main/java/com/frequency/bank/dtos/CustomerDto.java
@@ -1,12 +1,15 @@
 package com.frequency.bank.dtos;
 
 
+import java.util.UUID;
+
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
 public class CustomerDto {
+	private UUID customerId;
 	private String firstName;
 	private String lastName;
 	private String email;

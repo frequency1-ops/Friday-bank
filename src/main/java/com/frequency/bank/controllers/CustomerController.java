def664bc78b046762543f12f8c91346dedd5d83d
Salim_Juma_001 <87000813+frequency1-ops@users.noreply.github.com>
2025-11-16 20:09:24 +0300

    added deleting customers endpoint

--- src/main/java/com/frequency/bank/controllers/CustomerController.java
+++ src/main/java/com/frequency/bank/controllers/CustomerController.java
@@ -3,6 +3,7 @@ package com.frequency.bank.controllers;
 import java.util.UUID;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -66,5 +67,15 @@ public class CustomerController {
 		customerMapper.updateCustomer(request, customer);
 		customerRepository.save(customer);
 		return ResponseEntity.ok(customerMapper.toDto(customer));
+	}
+	@DeleteMapping("/{id}")
+	public ResponseEntity<Void> deleteCustomer(
+			@PathVariable(name = "id") UUID customerId
+			){
+		
+		customerRepository.findById(customerId).orElseThrow(CustomerNotFoundException::new);
+		customerRepository.deleteById(customerId);
+		
+		return ResponseEntity.noContent().build();
 	}
 }

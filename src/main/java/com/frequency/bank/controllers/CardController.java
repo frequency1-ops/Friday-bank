b3dfc52b6e2c7b8abc1eb75d96a3a307c7468491
Salim_Juma_001 <87000813+frequency1-ops@users.noreply.github.com>
2026-02-17 19:33:09 +0300

    initialised the card endpoints by starting with the get a card endpoint

--- src/main/java/com/frequency/bank/controllers/CardController.java
+++ src/main/java/com/frequency/bank/controllers/CardController.java
@@ -1,13 +1,15 @@
 package com.frequency.bank.controllers;
 
+import java.util.UUID;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.frequency.bank.dtos.CardDto;
 import com.frequency.bank.mappers.CardMapper;
-import com.frequency.bank.repositories.AccountRepository;
 import com.frequency.bank.repositories.CardRepository;
 
 import lombok.AllArgsConstructor;
@@ -24,6 +26,14 @@ public class CardController {
 	public ResponseEntity<Iterable<CardDto>> getAllCards(){
 		return ResponseEntity.ok(cardRepository.findAll()
 				.stream().map(cardMapper::toDto).toList());
+	}
+	
+	@GetMapping("/{id}")
+	public ResponseEntity<CardDto> getCard(
+			@PathVariable(name = "id") UUID cardId
+			){
+		var card = cardRepository.findById(cardId).orElseThrow();
+		return ResponseEntity.ok(cardMapper.toDto(card));
 	}
 	
 	

70494b3a28675d61d6789f3a72cb7a851d6999f7
Salim_Juma_001 <87000813+frequency1-ops@users.noreply.github.com>
2026-02-19 17:16:55 +0300

    Finished Transaction Dto and request

--- src/main/java/com/frequency/bank/dtos/TransactionDto.java
+++ src/main/java/com/frequency/bank/dtos/TransactionDto.java
@@ -1,5 +1,22 @@
 package com.frequency.bank.dtos;
 
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.UUID;
+
+import com.frequency.bank.entities.TransactionType;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class TransactionDto {
+	
+	private UUID transactionId;
+	private TransactionType transactionType;
+	private BigDecimal amount;
+	private String description;
+	private LocalDate timeStamp;
 
 }
--- src/main/java/com/frequency/bank/dtos/TransactionRequest.java
+++ src/main/java/com/frequency/bank/dtos/TransactionRequest.java
@@ -1,5 +1,27 @@
 package com.frequency.bank.dtos;
 
+import java.math.BigDecimal;
+
+import com.frequency.bank.entities.TransactionType;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class TransactionRequest {
+	
+	@NotNull
+	private TransactionType type;
+	
+	@NotNull
+	@Positive
+	private BigDecimal amount;
+	
+	@NotBlank
+	private String description;
 
 }

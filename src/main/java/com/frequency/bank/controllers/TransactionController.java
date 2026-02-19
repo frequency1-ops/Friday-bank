45eaf2af13dc3c4f7cc8839dbc8e58bca712933e
Salim_Juma_001 <87000813+frequency1-ops@users.noreply.github.com>
2026-02-19 16:59:34 +0300

    Still working on transactions

--- src/main/java/com/frequency/bank/controllers/TransactionController.java
+++ src/main/java/com/frequency/bank/controllers/TransactionController.java
@@ -1,9 +1,19 @@
 package com.frequency.bank.controllers;
 
+import java.util.UUID;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.frequency.bank.dtos.TransactionDto;
+import com.frequency.bank.dtos.TransactionRequest;
 import com.frequency.bank.mappers.TransactionMapper;
+import com.frequency.bank.repositories.AccountRepository;
 import com.frequency.bank.repositories.TransactionRepository;
 
 import lombok.AllArgsConstructor;
@@ -15,4 +25,29 @@ public class TransactionController {
 	
 	private final TransactionRepository transactionRepository;
 	private final TransactionMapper transactionMapper;
+	private final AccountRepository accountRepository;
+	
+	@GetMapping
+	public ResponseEntity<Iterable<TransactionDto>> getAllTransactions(){
+		
+		return ResponseEntity.ok(transactionRepository.findAll()
+				.stream().
+				map(transactionMapper::toDto).toList());
+	}
+	@GetMapping("/{id}")
+	public ResponseEntity<TransactionDto> getTransaction(
+			@PathVariable(name = "id") UUID transactionId
+			){
+		var transaction = transactionRepository.findById(transactionId).orElseThrow();
+		return ResponseEntity.ok(transactionMapper.toDto(transaction));
+	}
+	@PostMapping("/{id}")
+	public ResponseEntity<TransactionDto> createTransaction(
+			@PathVariable(name = "id") UUID accountId,
+			@RequestBody TransactionRequest request
+			){
+		var account = accountRepository.findById(accountId).orElseThrow();
+		var transaction = transactionMapper.toEntity(request);
+		return null;
+	}
 }

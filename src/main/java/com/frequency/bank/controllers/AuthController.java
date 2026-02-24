a05a76c3f7458c273a4361ca1423aa86bbddc2fe
Salim_Juma_001 <87000813+frequency1-ops@users.noreply.github.com>
2026-02-25 00:18:21 +0300

    Still working on authentication

--- src/main/java/com/frequency/bank/controllers/AuthController.java
+++ src/main/java/com/frequency/bank/controllers/AuthController.java
@@ -2,6 +2,7 @@ package com.frequency.bank.controllers;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,7 +22,7 @@ public class AuthController {
 	
 	public ResponseEntity<Void> login(@Valid @RequestBody LoginRequest request){
 		
-		authenticationManager.authenticate(null);
+		authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(request.getEmail(), request.getPassword()));
 		
 		return ResponseEntity.ok().build();
 		
